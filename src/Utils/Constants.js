9d3278430bdceaecc3ae34fe7defdbc32c8392cb
Hu, Xiaohui <xiaohuhu@stubhub.com>
2020-06-07 22:45:29 +0800

    服务器迁移

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -2,7 +2,8 @@
 export const Constants = {
     
     // APIBaseUrl: process.env.NODE_ENV === "production"? "http://106.75.216.135:8004/" : "https://localhost:44362/",
-    APIBaseUrl: process.env.NODE_ENV === "production"? "http://106.75.216.135:8004/" : "https://localhost:44362/",
-    ResourceUrl: process.env.NODE_ENV === "production"? "http://app.kangfupanda.com/Upload/" : "https://localhost:44362/Upload/",
-    OfficialSiteResourceUrl: process.env.NODE_ENV === "production"? "http://app.kangfupanda.com/Site/" : "https://localhost:44362/Site/",
+    APIBaseUrl: process.env.NODE_ENV === "production"? "http://47.111.166.154:8004/" : "https://localhost:44362/",
+    ResourceUrl: process.env.NODE_ENV === "production"? "http://47.111.166.154:8004/Upload/" : "https://localhost:44362/Upload/",
+    //OfficialSiteResourceUrl: process.env.NODE_ENV === "production"? "http://app.kangfupanda.com/Site/" : "https://localhost:44362/Site/",
+    OfficialSiteResourceUrl: process.env.NODE_ENV === "production"? "http://47.111.166.154:8004/Site/" : "https://localhost:44362/Site/",
 }
