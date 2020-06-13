48ecf38ab888dfa66a6d0ae226746b0b8059d56f
Hu, Xiaohui <xiaohuhu@stubhub.com>
2020-06-13 22:55:49 +0800

    修改web.config配置，为了生产环境的Delete请求问题

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -2,11 +2,11 @@
 export const Constants = {
     
     // APIBaseUrl: process.env.NODE_ENV === "production"? "http://106.75.216.135:8004/" : "https://localhost:44362/",
-    // APIBaseUrl: process.env.NODE_ENV === "production"? "https://api.kangfupanda.com/" : "https://localhost:44362/",
-    // ResourceUrl: process.env.NODE_ENV === "production"? "https://api.kangfupanda.com/Upload/" : "https://localhost:44362/Upload/",
-    APIBaseUrl: process.env.NODE_ENV === "production"? "http://47.111.166.154:8004/" : "https://localhost:44362/",
-    ResourceUrl: process.env.NODE_ENV === "production"? "http://47.111.166.154:8004/Upload/" : "https://localhost:44362/Upload/",
+    APIBaseUrl: process.env.NODE_ENV === "production"? "https://api.kangfupanda.com/" : "https://localhost:44362/",
+    ResourceUrl: process.env.NODE_ENV === "production"? "https://api.kangfupanda.com/Upload/" : "https://localhost:44362/Upload/",
+    //APIBaseUrl: process.env.NODE_ENV === "production"? "http://47.111.166.154:8004/" : "https://localhost:44362/",
+    //ResourceUrl: process.env.NODE_ENV === "production"? "http://47.111.166.154:8004/Upload/" : "https://localhost:44362/Upload/",
     //OfficialSiteResourceUrl: process.env.NODE_ENV === "production"? "http://app.kangfupanda.com/Site/" : "https://localhost:44362/Site/",
-    //OfficialSiteResourceUrl: process.env.NODE_ENV === "production"? "https://api.kangfupanda.com/Site/" : "https://localhost:44362/Site/",
-    OfficialSiteResourceUrl: process.env.NODE_ENV === "production"? "http://47.111.166.154:8004/Site/" : "https://localhost:44362/Site/",
+    OfficialSiteResourceUrl: process.env.NODE_ENV === "production"? "https://api.kangfupanda.com/Site/" : "https://localhost:44362/Site/",
+    //OfficialSiteResourceUrl: process.env.NODE_ENV === "production"? "http://47.111.166.154:8004/Site/" : "https://localhost:44362/Site/",
 }
